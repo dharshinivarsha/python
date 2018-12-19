1c990e285bc56efa0a31b0f02de9a12c7f46969c
dharshinivarsha <42231123+dharshinivarsha@users.noreply.github.com>
2018-12-19 14:48:29 +0530

    Create set2factorial.py

---
+++ set2factorial.py
@@ -0,0 +1,8 @@
+if num <0:
+print("factorial does not exists for negative number")
+elif num==:
+print("the factorial of 0 is 1")
+else;
+for i in range(1,num+1)
+factorial=factorial*i
+print("the factorial of", num,"is',factorial)
