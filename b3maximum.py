13428256a2da3bc9b32c8f2ae4592876e576aa1d
dharshinivarsha <42231123+dharshinivarsha@users.noreply.github.com>
2019-01-11 10:45:52 +0530

    Create b3maximum.py

---
+++ b3maximum.py
@@ -0,0 +1,9 @@
+num1=float(input("enter the first element:"));
+num2=float(input("enter the second element:"));
+num3=float(input("enter the third element:"));
+if(num1>=num2)and (num1>=num3)
+maximum element=num1
+elif(num2>=num1)and (num2>=num3)
+maximum element=num1
+else
+print("the maximum element:'")
