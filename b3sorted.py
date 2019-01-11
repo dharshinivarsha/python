1c1bc5d8c7d2a2bc5b4c9506b7677814c65e2e75
dharshinivarsha <42231123+dharshinivarsha@users.noreply.github.com>
2019-01-11 12:09:34 +0530

    Update and rename b3sort.py to b3sorted.py

---
+++ b3sorted.py
@@ -0,0 +1,12 @@
+numbers = [1, 3, 4, 2] 
+numbers.sort(reverse=True) 
+  print(numbers) 
+decimalnumber = [2.01, 2.00, 3.67, 3.28, 1.68] 
+decimalnumber.sort(reverse=True) 
+  print(decimalnumber) 
+words = ["Geeks", "For", "Geeks"]  
+words.sort(reverse=True) 
+  print(words) # List of Integers 
+numbers = [1, 3, 4, 2] 
+  
+
