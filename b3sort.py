d610ca4514a8e48c39d7e98418f1140d87579427
dharshinivarsha <42231123+dharshinivarsha@users.noreply.github.com>
2019-01-11 11:57:15 +0530

    Create b3sort.py

---
+++ b3sort.py
@@ -0,0 +1,10 @@
+vowels = ['e', 'a', 'u', 'o', 'i']
+vowels.sort()
+print('Sorted list:', vowels)# vowels list
+vowels = ['e', 'a', 'u', 'o', 'i']
+
+# sort the vowels
+vowels.sort()
+
+# print vowels
+print('Sorted list:', vowels)
