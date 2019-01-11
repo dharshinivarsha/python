b8f2ff191a224b08cd383190ee7d0d4bd57f7a01
dharshinivarsha <42231123+dharshinivarsha@users.noreply.github.com>
2019-01-11 10:29:50 +0530

    Create b3arithmetic.py

---
+++ b3arithmetic.py
@@ -0,0 +1,7 @@
+def Nth of_AP(N, A, D) : 
+     print( "The ", N ,"th term of the series is : ", 
+       Nth_of_AP(N,A,D)def Nth of_AP(N, A, D) : 
+     print( "The ", N ,"th term of the series is : ", 
+       Nth_of_AP(N,A,D)def Nth of_AP(N, A, D) : 
+     print( "The ", N ,"th term of the series is : ", 
+       Nth_of_AP(N,A,D)
